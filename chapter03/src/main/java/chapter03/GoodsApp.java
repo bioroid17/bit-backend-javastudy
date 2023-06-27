97c71b3bfd9085115dcad2b8811ae64bac6fd177
JeongHoon Lee <trace1997@ajou.ac.kr>
2023-06-27 15:46:15 +0900

    Java method

--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -5,15 +5,20 @@ public class GoodsApp {
 	public static void main(String[] args) {
 		Goods camera = new Goods();	// 기본 생성자
 		System.out.println("상품 갯수:"+Goods.countOfGoods);
+		
 		camera.setName("nikon");
 		camera.setPrice(400000);
 		camera.setCountStock(20);
 		camera.setCountSold(40);
 
-		System.out.println("이름:"+camera.getName());
-		System.out.println("가격:"+camera.getPrice());
-		System.out.println("재고:"+camera.getCountStock());
-		System.out.println("판매량:"+camera.getCountSold());
+//		System.out.println("이름:"+camera.getName());
+//		System.out.println("가격:"+camera.getPrice());
+//		System.out.println("재고:"+camera.getCountStock());
+//		System.out.println("판매량:"+camera.getCountSold());
+		camera.showInfo();
+
+		int discountPrice = camera.calcDiscountPrice(0.5);
+		System.out.println(discountPrice);
 
 		Goods goods2 = new Goods();	// 기본 생성자
 		System.out.println("상품 갯수:"+Goods.countOfGoods);
