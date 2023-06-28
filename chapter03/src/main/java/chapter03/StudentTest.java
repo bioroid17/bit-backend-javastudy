9ce46c89f2eda27cc66c251dbd623ac9833234dd
JeongHoon Lee <trace1997@ajou.ac.kr>
2023-06-28 16:52:28 +0900

    Java 상속/다형성 - 오버라이드 (주석 수정)

--- chapter03/src/main/java/chapter03/StudentTest.java
+++ chapter03/src/main/java/chapter03/StudentTest.java
@@ -7,10 +7,12 @@ public class StudentTest {
 		Student s1 = new Student();
 		s1.setGrade(1);
 		
-		Person p1 = s1;	// 업캐스팅(Upcasting). 암시적(implicitly) 캐스팅 가능하다.
+		// Upcasting: 자식 클래스가 부모 클래스 타입으로 변환되는 것. 암시적(implicitly) 캐스팅 가능하다.
+		Person p1 = s1;
 		p1.setName("둘리");
 		
-		Student s2 = (Student) p1;	// 다운개스팅(Dowmcasting). 명시적(explicitly) 캐스팅만 가능하다.
+		// Downcasting: 업캐스팅 된 것을 원래대로 되돌린다. 명시적(explicitly) 캐스팅만 가능하다.
+		Student s2 = (Student) p1;
 		s2.setMajor("Computer Science");
 		
 		// 괄호로 p1을 묶지 않으면 setMajor의 리턴 타입인 null을 Student로 캐스팅 하려고 해서 오류 발생
