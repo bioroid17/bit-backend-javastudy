e8aaaec273d13cb303c8dd21ececa01fc5b96e8c
bioroid17 <trace1997@ajou.ac.kr>
2023-06-30 15:52:15 +0900

    Java 예외 처리

--- chapter03/src/main/java/exception/MyClassTest.java
+++ chapter03/src/main/java/exception/MyClassTest.java
@@ -3,9 +3,8 @@ package exception;
 public class MyClassTest {
 
 	public static void main(String[] args) {
-		MyClass myClass = new MyClass();
-		
 		try {
+			MyClass myClass = new MyClass();
 			myClass.danger();
 		} catch (MyException e) {
 			// TODO Auto-generated catch block
