ea6ea2d44c60ad5adf224e944078837a1a5b2736
bioroid17 <trace1997@ajou.ac.kr>
2023-07-03 10:06:34 +0900

    chapter 03 Java 기본 구문 객체지향 프로그래밍 - 답 수정

--- practice04/src/main/java/prob04/Depart.java
+++ practice04/src/main/java/prob04/Depart.java
@@ -8,6 +8,14 @@ public class Depart extends Employee {
 		this.department = department;
 	}
 	
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
 	@Override
 	public void getInformation() {
 		System.out.println( "이름: " + getName() + "\t연봉: " + getSalary() + "\t부서: " + department );
